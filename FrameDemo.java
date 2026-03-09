7017de47ad5faf387c69be2fc8b5c0a498d19104
IbrahimS666 <anwaribrahim0873@gmail.com>
2026-03-09 12:25:23 +0530

    Add files via upload

---
+++ FrameDemo.java
@@ -0,0 +1,11 @@
+
+import java.awt.*;
+
+class FrameDemo {
+    public static void main(String args[]) {
+        Frame f = new Frame();
+        f.setTitle("ibrahim-bca");
+        f.setSize(500, 400);
+        f.setVisible(true);
+    }
+}
